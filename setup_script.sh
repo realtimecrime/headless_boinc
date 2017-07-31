5ae0a4495bd88fab5ac927bb34864773264eb502
realtimecrime <jonenone@gmail.com>
2017-07-31 17:55:09 +0200

    Error fixed

--- setup_script.sh
+++ setup_script.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install boinc boinctui
+sudo apt-get install -y boinc boinctui
 
 mv global_prefs_override.xml /etc/boinc-client/global_prefs_override.xml
 boinccmd --read_global_prefs_override
@@ -10,4 +10,4 @@ boinccmd --join_acct_mgr https://www.grcpool.com jonenone $password
 boinccmd --set_run_mode always
 boinccmd --set_network_mode always
 
-nohup ./restart_script.sh >log.out &
+nohup ./restart_boinc.sh >log.out &
